3999af65ddb7bd5b12c563c7029d5d6c4fa44dd3
Jacy Wang <jsycwangwei@icloud.com>
2016-02-02 20:15:18 +0800

    UI

--- jmx-front/src/main/java/com/focustech/jmx/web/controller/JmxController.java
+++ jmx-front/src/main/java/com/focustech/jmx/web/controller/JmxController.java
@@ -1,20 +1,49 @@
 package com.focustech.jmx.web.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.focustech.jmx.po.JmxApplication;
+import com.focustech.jmx.po.JmxServer;
+import com.google.common.collect.Lists;
 
 @Controller
 public class JmxController {
 	@RequestMapping(value = "/index.html")
     public String index(Model model, String server, String app) {
         model.addAttribute("common", "1");
+        List<JmxApplication> list = Lists.newArrayList();
+        JmxApplication a = new JmxApplication();
+        a.setAppId(1);
+        a.setAppName("1");
+        JmxApplication b = new JmxApplication();
+        b.setAppId(2);
+        b.setAppName("2");
+        list.add(a);
+        list.add(b);
+        model.addAttribute("list", list);
         return "/new/index";
     }
 	
-	@RequestMapping(value = "/detail.html")
-    public String detail(Model model, String server, String app) {
+	@RequestMapping(value = "/jmx0/getHosts")
+	@ResponseBody
+    public List<JmxServer> detail(Model model, String appId) {
+		JmxServer s1 = new JmxServer();
+		s1.setHostId(1);
+		s1.setHostIp("192.168.0.1");
+		JmxServer s2 = new JmxServer();
+		s2.setHostId(2);
+		s2.setHostIp("192.168.0.2");
+		List<JmxServer> l = Lists.newArrayList();
+		l.add(s1);
+		l.add(s2);
+		model.addAttribute("list",l);
         model.addAttribute("common", "1");
-        return "/new/detail";
+        return l;
     }
 }
+
--- jmx-front/src/main/webapp/resources/js/new/index.js
+++ jmx-front/src/main/webapp/resources/js/new/index.js
@@ -1,3 +1,17 @@
 /**
  * Created by wangwei-ww on 2016/1/29.
  */
+$(".arrow").on("click",function (e){
+	$.ajax({
+		url: "/jmx0/getHosts",
+		type: "POST",
+		cache: false,
+		data: {"appId" : $(this).attr("data-id")}, 
+		success: function(data){
+			alert(_.template($('#hosts').html(), data));
+		},
+		error: function(data){
+			alert("err");
+		}
+	});
+});
